cc07da07f5783193f9aa4ba2ef758d08b94d7e64
Kelly Jordan <jordakel@sheridancollege.ca>
2024-05-02 01:39:41 -0400

    added in the buttons and the pomodoro label counter

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from tkinter import ttk, PhotoImage
 #UI class
 class PomodoroTimer:
     def __init__(self):
+        #UI
         self.root = tk.Tk()
         self.root.geometry("600x300")
         self.root.title("Pomodoro Timer")
@@ -42,8 +43,23 @@ class PomodoroTimer:
         self.tabs.add(self.tab1, text="Pomodoro")
         self.tabs.add(self.tab2, text="Short break")
         self.tabs.add(self.tab3, text="Long Break")
-
+        #grid layout
+        self.grid_layout = ttk.Frame(self.root) #adds the  grid layout to the root (main UI)
+        self.grid_layout.pack(pady=10) #grid layout is packed into the root frame
         #adding in the buttons 
+
+        self.start_button = ttk.Button(self.grid_layout, text="Start", command=self.start_timer_thread) #adds into the grid layout and the command is tied to the self.start_timer thread
+        self.start_button.grid(row=0, column=0)
+
+        self.skip_button = ttk.Button(self.grid_layout, text="Skip", command=self.skip_clock) #adds into the grid layout and the command is tied to the self.start_timer thread
+        self.skip_button.grid(row=0, column=1)
+
+        self.reset_button = ttk.Button(self.grid_layout, text="Reset", command=self.reset_timer) #adds into the grid layout and the command is tied to the self.start_timer thread
+        self.reset_button.grid(row=0, column=2)
+
+        #adding the label count for the pormodo
+        self.pomodoro_counter_label = ttk.Label(self.grid_layout, text="Pomodoro: 0", font=("Ubuntu",16))
+        self.pomodoro_counter_label.grid(row=1, column=0, columnspan=3) #columnspan adjusted the position of the column?
 
 
 
